179f934245340dbc7d48b61d01b86c195b1f6d4a
Bautista Cremona <bautistacremona@gmail.com>
2025-08-26 02:43:15 -0300

    images in card

--- client/src/components/PropertyCard.jsx
+++ client/src/components/PropertyCard.jsx
@@ -9,21 +9,26 @@ export default function PropertyCard({ property }) {
 
   return (
     <Link to={`/properties/${property.id}`} >
-    <div className="border rounded-lg p-4 shadow-md mb-3">
-      <h2 className="text-lg font-bold">{property.title}</h2>
-      <p className="text-gray-600">{property.location}</p>
-      <p className="text-blue-600 font-semibold">
-        ${property.rentAmount} / month
-      </p>
-      <p className="text-sm text-gray-500">{property.leaseType}</p>
-      {user.role === "landlord" && user.id === property.landlordId && (
-        <>
-        <Link to={`/properties/edit/${property.id}`} > {/* create enpoint to edit property */}
-          <button className="text-white bg-sky-700 hover:bg-sky-600 hover:scale-110 transition-all px-3 py-1 rounded-md mr-2 inline-block mt-2">Edit Property</button>
-        </Link>
-          <button onClick={() => deleteProperty(property.id)} className="text-white bg-red-700 hover:bg-red-600 hover:scale-110 transition-all px-3 py-1 rounded-md mr-2 inline-block mt-2">Delete Property</button>
-        </>
-      )}
+    <div className="border rounded-lg p-4 shadow-md mb-3 flex flex-row">
+      <div className="w-1/3 mr-4">
+        <img src={property.imageUrl} alt={property.title} className="w-full h-48 object-cover mb-4 rounded-md" />
+      </div>
+      <div>
+        <h2 className="text-2xl font-bold">{property.title}</h2>
+        <p className="text-lg text-gray-600">{property.location}</p>
+        <p className="text-lg text-blue-600 font-semibold">
+          ${property.rentAmount} <span className="text-gray-600">/ month</span>
+        </p>
+        <p className="text-md text-gray-500">{property.leaseType}</p>
+        {user.role === "landlord" && user.id === property.landlordId && (
+          <>
+          <Link to={`/properties/edit/${property.id}`} > {/* create enpoint to edit property */}
+            <button className="text-white bg-sky-700 hover:bg-sky-600 hover:scale-110 transition-all px-3 py-1 rounded-md mr-2 inline-block mt-2">Edit Property</button>
+          </Link>
+            <button onClick={() => deleteProperty(property.id)} className="text-white bg-red-700 hover:bg-red-600 hover:scale-110 transition-all px-3 py-1 rounded-md mr-2 inline-block mt-2">Delete Property</button>
+          </>
+        )}
+      </div>
     </div>
     </Link>
   );
